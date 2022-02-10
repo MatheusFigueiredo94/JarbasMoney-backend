21d5077ca58e59ba726ff43375098f6abf0999a7
Matheus Figueiredo <matheus_gandolfofig@hotmail.com>
2022-02-10 00:48:59 -0300

    Adicionar arquivos de projeto.

---
+++ JarbasMoney.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JarbasMoney.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ JarbasMoney.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JarbasMoney.Shared
+{
+    public class Class1
+    {
+    }
+}
